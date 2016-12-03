5b541828a0d60765b29665ee77e5c238eb03d8d5
Sarina <ssinick@calstate.edu>
2016-12-03 02:23:00 -0800

    Completed successful test of location swap.  Pulling item via the API, parsing the item location and placing a PUT request with the item XML that contains swapped location information

--- update_locations.py
+++ update_locations.py
@@ -3,6 +3,7 @@ import requests
 import sys
 import csv
 import ConfigParser
+import logging
 import xml.etree.ElementTree as ET
 
 
@@ -17,6 +18,9 @@ def get_sru_base():
 	
 def get_base_url():
 	return config.get('Params', 'baseurl')
+
+def get_location_mapping():
+	return config.get('Params', 'locations')
 	
 
 
@@ -39,27 +43,89 @@ def read_location_mapping(loc_map_file):
 
 
 """
-	Read in item csv file (all items in Alma)?
+	Read in item csv file of items exported from Millennium/Sierra that contain a SAVE ITEM field
 """
+def read_items(item_file):
+	f  = open(item_file,'rt')
+	try:
+		reader = csv.reader(f)
+		header = reader.next()
+		locations = read_location_mapping(get_location_mapping())
+		for row in reader:
+			print row
+			parse_row(row,locations)
+
+	finally:
+		f.close()
 
 
 """
-	Read in save item csv mapping based on item barcode
+	Makes PUT request for item with updated location and temporary location information	
+"""	
+def post_item(item,barcode):
+	item_url =  item.attrib['link'] + "?apikey=" +  get_key()
+	headers = {"Content-Type": "application/xml"}
+	r = requests.put(item_url,data=ET.tostring(item),headers=headers)
+	print r.content
+
+	
 """
+	Returns the item's permanent location from the SAVE ITEM field
+"""	
+def get_permanent_location(save_item_info):
+	olocat = save_item_info.split(',')[1]
+	loc = olocat.split('=')[1]
+	return loc.strip()
 
 """
-	Search SRU for item barcode, return matching bib MMS ID 
+	Parse item data
 """
+def parse_row(row,locations):
+	barcode = row[0]
+	save_item_info = row[2]
+	permanent_location = get_permanent_location(save_item_info)
+	item = get_item_xml(barcode)
+	if item is not None:
+		if item.find("holding_data/in_temp_location").text != 'true':
+			temp_location = item.find("item_data/location").text
+			temp_library = item.find("item_data/library").text
+			temp_location_desc = item.find("item_data/location").attrib['desc']
+			temp_library_desc = item.find("item_data/library").attrib['desc']
+			new_temp_library = item.find("holding_data/temp_library")
+			new_temp_location = item.find("holding_data/temp_location")
+			in_temp_location = item.find("holding_data/in_temp_location")
+			in_temp_location.text =  "true"
+			new_temp_location.text = temp_location
+			new_temp_library.text = temp_library
+			perm_location = item.find("item_data/location")
+			perm_library = item.find("item_data/library")
+			perm_location.text = locations[permanent_location]['location']
+			perm_library.text = locations[permanent_location]['library']
+			print ET.tostring(item)
+			post_item(item,barcode)
+		else:
+			logging.info("Item already in temporary location: " + barcode)
+
 
 """
 	Get item info based on item barcode
 """
 def get_item_xml(barcode):
 	item_url = get_base_url() + "/items?item_barcode=" + barcode +  "&apikey=" + get_key()
-	print item_url
+	response = requests.get(item_url)
+	if response.status_code != 200:
+		logging.info("Item not found for item barcode: " + barcode)
+		return None
+	print ET.fromstring(response.content)
+	item = ET.fromstring(response.content)
+	return item
 
 
-get_item_xml()
+config = ConfigParser.RawConfigParser()
+config.read(sys.argv[1])
+logging.basicConfig(filename='status.log',level=logging.DEBUG)
+items_file = sys.argv[2]
+read_items(items_file)
 
 
 
